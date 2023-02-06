bf76bfcdac38bdcc8af7c0516ba3d21bc8e14151
[Anthony] <stine.anthony@yahoo.com>
2023-02-06 14:22:21 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+
+    String name = "Anthony";
+    int age = 32;
+    String hometown = "New Haven, CT";
+
+    System.out.println(String.format("My name is %s", name));
+    System.out.println(String.format("I am %d years old", age));
+    System.out.println(String.format("My hometown is %s", hometown));
+  }
+}
